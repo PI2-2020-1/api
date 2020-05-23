8c1e442bc4f65aa56eb5ca22c97cacf4120c1d9d
Thiago Pereira <thiagorpereira7@gmail.com>
2020-05-22 23:30:05 -0300

    Docker
    Co-authored-by: Thiago Pereira <thiagorpereira7@gmail.com>
    Co-authored-by: Vinicius Oliveira <vinifladf@gmail.com>"

--- sics/settings.py
+++ sics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '4qa2d53r4y_gstf*pj-(qa0_2j#jwqlb6ab9j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'sics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
