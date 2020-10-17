44b4373dea48d1eac3a5ec76260d417c66926c6d
GeovanaRamos <geovannna.1998@gmail.com>
2020-10-16 22:17:03 -0300

    Adding populate

---
+++ populate.py
@@ -0,0 +1,119 @@
+import os.path
+import sys
+import random
+import string
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+
+def create_password():
+    return 'goiaba10'
+
+
+def create_super_user(username, email):
+
+    password = create_password()
+    try:
+        u = User.objects.create_superuser(username,
+                                          email,
+                                          password,
+                                          is_active=True)
+        EmailAddress.objects.create(
+            user=u, email=email,
+            primary=True, verified=True)
+        u.set_password(password)
+        u.save()
+        print ('\nSuperUser:', User.objects.get(is_superuser=True).username)
+        print('username: {0} -- password: {1} \n'.format(username, password))
+
+        return u
+
+    except IntegrityError:
+        raise ValidationError("An error occurred. Stopping the script")
+
+
+def create_user(full_name, telegram, username, email, cpf, is_responsible, is_active):
+
+    password = create_password()
+
+    try:
+        u = User.objects.create_user(
+            full_name=full_name,
+            telegram=telegram,
+            cpf=cpf,
+            is_responsible=is_responsible,
+            username=username,
+            email=email,
+            password=password,
+            is_active=is_active,
+        )
+
+        print('User: - {0} {1}'.
+              format(str(u.first_name), str(u.last_name)))
+        print('username: {0}  -- password: {1} \n'.format(username, password))
+
+        return u
+
+    except IntegrityError:
+        raise ValidationError("An error occurred. Stopping the script")
+
+
+def create_plantation(farm, name, responsible, employees):
+    try:
+        plantation = Plantation.objects.create(
+            farm=farm,
+            name=name,
+            responsible=responsible,
+        )
+        for e in employees:
+            plantation.employees.add(e)
+        plantation.save()
+    except IntegrityError:
+        raise ValidationError("An error occurred. Stopping the script")
+
+
+
+def populate():
+    print ('\n----------------------')
+    print ('Populating Database...')
+    print ('----------------------\n')
+
+    create_super_user('admin', 'admin@admin.com')
+    
+    user_1 = create_user(
+        full_name='Geovana Ramos Sousa Silva', 
+        telegram='Geovana_RMS', 
+        username='geovana',
+        email='geovana@email.com',
+        cpf='111111111111',
+        is_responsible=True,
+        is_active=True
+    )
+
+    user_2 = create_user(
+        full_name='Gabriela Medeiros da Silva', 
+        telegram='g_msilva', 
+        username='gabriela',
+        email='gabriela@email.com',
+        cpf='222222222222',
+        is_responsible=False,
+        is_active=True
+    )
+    
+    create_plantation('Rancho Bom', 'Milho', user_1, [user_2])
+
+    print ('\n------------------------------\n')
+    print ('Database populated with sucess')
+    print ('------------------------------\n')
+
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sics.settings')
+django.setup()
+from django.utils import timezone
+from sics.api.models import User, Plantation
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from allauth.account.models import EmailAddress
+
+populate()
