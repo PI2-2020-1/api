7de3a101dd6efd16067d2e17b056aadb68f47ca4
GeovanaRamos <geovannna.1998@gmail.com>
2020-11-06 21:00:25 -0300

    Adding station to admin

--- sics/api/admin.py
+++ sics/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Plantation, Parameter, Reading
+from .models import User, Plantation, Parameter, Reading, Station
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Plantation)
 admin.site.register(Parameter)
 admin.site.register(Reading)
+admin.site.register(Station)
